44d52394ac35d7ebf8906d5c1380f5d708b56de1
Geoffrey Belcher <geoff.belcher@ontailwind.com>
2026-03-10 17:30:15 -0400

    Updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.prisma" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
